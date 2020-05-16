0179da17c61bd6e5f631d975d576cb0554083b7d
Martin Ruiz <martin.ruiz.mares@gmail.com>
2020-05-16 00:29:38 -0500

    fix

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/bookmarksdb';
+exports.DATABASE_URL = 'mongodb+srv://admin:Wwewwe333.@cluster0-zwqhy.mongodb.net/bookmarksdb?retryWrites=true&w=majority';
 exports.TOKEN = process.env.API_KEY || '2abbf7c3-245b-404f-9473-ade729ed4653';
 exports.PORT = process.env.PORT || '8000';
